809a9b475e9f91fc00405ac56405660195157ddf
marineco <marinecolonge@gmail.com>
2016-09-30 10:56:27 +0200

    Nouvel exercice jquery form capture Création bouton + récupération diff valeurs + afficher objet dans console + test ok

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log('have fun !');
+
+/* Form Capture
+
+Récuperez les valeurs des champs du formulaire et en faire un joli 
+objet qui sera affiché dans la console. */
+
+$("button").click(function(){
+	// alert('ok');
+
+	var val1 = $('#first_name').val();
+	alert('Mon prénom est ' + val1);
+
+	var val2 = $('#last_name').val();
+	alert('Mon nom est ' + val2);
+
+	var val3 = $('#city').val();
+	alert('Je vis à ' + val3);
+
+	var nom = {
+		first_name : "val1",
+		last_name : "val2",
+		city : "val3"
+	};
+
+	console.log(nom)
+});
+
+
+/* Concaténez les champs first_name et last_name et dites bonjour à 
+notre cher visiteur dans le titre en replacant le contenu du 
+span #username */
+
