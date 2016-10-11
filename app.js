930966d67773fdec7d96c342f56c1a1ee77d9935
marineco <marinecolonge@gmail.com>
2016-10-11 22:40:43 +0200

    Nettoyage de mon code Retirer les console.log / enlever les instructions commentées

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-console.log('have fun !');
-
-/* Form Capture
-
-Récuperez les valeurs des champs du formulaire et en faire un joli 
-objet qui sera affiché dans la console. */
 
 $("button").click(function(){
-	// alert('ok');
 
 	var val1 = $('#first_name').val();
 	alert('Mon prénom est ' + val1);
@@ -23,15 +16,7 @@ $("button").click(function(){
 		city : val3
 	};
 
-	console.log(nom)
-
-
 	var bonjour = (nom.first_name + ' ' + nom.last_name);
 
 	$("#username").html(bonjour);
 });
-
-
-/* Concaténez les champs first_name et last_name et dites bonjour à 
-notre cher visiteur dans le titre en replacant le contenu du 
-span #username */
